7bc7dc5855479f48239cb1a3fdafc16b42f65fe9
dmitryi.savenko <dmitryi.savenko>
2021-09-21 15:19:15 +0300

    PP-27 Admin Offer page

--- admin/template/project/index.php
+++ admin/template/project/index.php
@@ -1,2 +1,33 @@
-<?php
-var_dump($PROJECTS);
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-primary">
+
+                <div class="box-body">
+
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th class="text-center">Preview</th>
+                            <th class="text-center">Offer name</th>
+                            <th class="text-center">Website Url</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        <tr>
+                            <td class="text-center">1</td>
+                            <td class="text-center">2</td>
+                            <td class="text-center">3</td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</section>
--- admin/template/project/view.php
+++ admin/template/project/view.php
@@ -1,2 +1,33 @@
-<?php
-var_dump($PROJECT);
+<section class="content">
+    <div class="center-block" style="margin-bottom: 20px; max-width: 1280px; max-height: 768px">
+        <img class="img-responsive" src="/assets_admin/img/1920x1080-px-BMW-BMW-E36-car-681106.jpg" alt="">
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-primary">
+
+                <div class="box-header with-border">
+                    <h3 class="box-title">Offer info</h3>
+                </div>
+
+                <div class="box-body">
+                    <table class="table table-bordered table-striped table-hover" style="width: 100%">
+                        <tbody>
+                        <tr>
+                            <td style="min-width: 30%">Website</td>
+                            <td class="text-bold">Site Name</td>
+                        </tr>
+                        <tr>
+                            <td>Url</td>
+                            <td class="text-bold">http://localhost:3003</td>
+                        </tr>
+
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
