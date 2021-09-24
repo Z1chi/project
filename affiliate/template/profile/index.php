ea9a5202e62f9f0b38a5d68dbcdaee3b14e230ed
Ivaniv Anton <ivaniv.tony@mail.ru>
2021-09-24 17:58:03 +0300

    - Added imagick php ext; - Added data to template;

--- affiliate/template/profile/index.php
+++ affiliate/template/profile/index.php
@@ -9,21 +9,24 @@
 
                 <div class="box-body">
                     <form method="post" class="js_profile_changes profile__form">
-<!--                        <img src="" alt="Profile avatar">-->
+                        <img src="<?= $AFFILIATE->img?>" style="width: 200px;" alt="Profile avatar">
                         <label for="img">Profile avatar</label>
                         <input type="file" id="img" name="img" class="form-control text-center color__default" value=""
                                placeholder="">
                         <br>
                         <label for="firstName">First Name</label>
-                        <input type="text" id="firstName" name="firstName" class="name form-control color__default" value=""
+                        <input type="text" id="firstName" name="firstName"
+                               class="name form-control color__default" value="<?= $AFFILIATE->first_name?>"
                                placeholder="John">
                         <br>
                         <label for="lastName">Last Name</label>
-                        <input type="text" id="lastName" name="lastName" class="name form-control color__default" value=""
+                        <input type="text" id="lastName" name="lastName" class="name form-control color__default"
+                               value="<?= $AFFILIATE->last_name?>"
                                placeholder="Deer">
                         <br>
                         <label for="email">Email</label>
-                        <input type="email" id="email" name="email" class="userEmail form-control color__default" value=""
+                        <input type="email" id="email" name="email" class="userEmail form-control color__default"
+                               value="<?= $AFFILIATE->email?>"
                                placeholder="john.d@email.com">
                         <br>
                         <label for="oldPassword">Old password</label>
@@ -40,10 +43,12 @@
                                placeholder="">
                         <br>
                         <label for="wallet">BTC wallet</label>
-                        <input type="text" id="wallet" name="wallet" class="wallet form-control color__default" value="" placeholder="">
+                        <input type="text" id="wallet" name="wallet" class="wallet form-control color__default"
+                               value="<?= $AFFILIATE->wallet?>" placeholder="">
                         <br>
                         <label for="telegram">Telegram</label>
-                        <input type="text" id="telegram" name="telegram" class="form-control" value=""
+                        <input type="text" id="telegram" name="telegram" class="form-control"
+                               value="<?= $AFFILIATE->telegram?>"
                                placeholder="">
                         <br>
                         <button type="submit" class="btn btn-success buttonSubmit">Save</button>
