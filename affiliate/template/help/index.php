ee11079ac0cd14eb886569edfcc36a7916ec7489
Ivaniv Anton <ivaniv.tony@mail.ru>
2021-09-30 18:08:59 +0300

    - Added collapsable block; - Added layout menu item;

--- affiliate/template/help/index.php
+++ affiliate/template/help/index.php
@@ -1,3 +1,46 @@
 <section class="content">
-    <h1>This is FAQ Page</h1>
+    <div class="panel-group" role="tablist" aria-multiselectable="true">
+        <div class="panel panel-default" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            <div class="panel-heading" role="tab" id="headingOne">
+                <h4 class="panel-title">
+                    <a>
+                        Collapsible Group Item #1
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae consequatur distinctio dolor doloribus ducimus ea eaque enim explicabo harum illo labore laborum necessitatibus officia placeat possimus quam quas reiciendis sunt tempora unde, velit veniam voluptatibus? Aliquam, corporis dicta iure magnam quis voluptates voluptatibus. Cum dolorum expedita id sit tempore?
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            <div class="panel-heading" role="tab" id="headingTwo">
+                <h4 class="panel-title">
+                    <a class="collapsed">
+                        Collapsible Group Item #2
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus atque, blanditiis debitis dolorem error esse facere id laboriosam nulla officiis perspiciatis quibusdam saepe suscipit. Commodi!
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            <div class="panel-heading" role="tab" id="headingThree">
+                <h4 class="panel-title">
+                    <a class="collapsed">
+                        Collapsible Group Item #3
+                    </a>
+                </h4>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                <div class="panel-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, tenetur.
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
--- affiliate/template/layout/default.php
+++ affiliate/template/layout/default.php
@@ -119,6 +119,11 @@
                         <i class="fa fa-bar-chart"></i> <span>Statistic</span>
                     </a>
                 </li>
+                <li>
+                    <a href="<?=App::createUrl('/help')?>">
+                        <i class="fa fa-question"></i> <span>FAQ</span>
+                    </a>
+                </li>
 				<li class="header">Contacts</li>
 
 				<li>
