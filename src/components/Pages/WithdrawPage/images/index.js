ee00998cfaa0dd446e9d44adf4b66dc5bcdfa9d0
kurpfish <kurpfesh@gmail.com>
2021-12-28 14:52:56 +0300

    withdraw drawer fixes

--- src/components/Pages/WithdrawPage/images/index.js
+++ src/components/Pages/WithdrawPage/images/index.js
@@ -1,7 +1,9 @@
 import balanceIcon from './balance.svg';
 import emptyTableIcon from './emptyTable.svg';
+import withdrawIcon from './withdraw.svg';
 
 export const images = {
     balanceIcon,
     emptyTableIcon,
+    withdrawIcon,
 }
