60d564b4053ea30ae306bf1281eac4cfb441c4ef
kurpfesh <kurpfesh@gmail.com>
2022-05-03 12:08:36 +0300

    PP-233 test dev mode

--- config/webpack/prod.js
+++ config/webpack/prod.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = merge(common, {
-    mode: 'production',
+    mode: 'development',
 
     entry: {
         index: {
